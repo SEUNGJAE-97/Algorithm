d581f87ec91b1a1350be1e8829df16c15f4f08d5
하승재 <117517535+SEUNGJAE-97@users.noreply.github.com>
2025-03-03 23:59:07 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 12216 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,33 +1,35 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	private static int N, M;
+	private static int[] numbers;
 
-  private static int N, M;
-  private static int[] numbers;
+	private static void combination(int cnt, int start) {
+		if (cnt == M) {
+			for (int num : numbers) {
+				System.out.print(num);
+				System.out.print(" ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = start; i <= N; i++) {
+			
+			numbers[cnt] = i;
+			combination(cnt + 1, i+1);
+		}
+	}
 
-  private static void combination(int depth, int start) {
-    if (depth == M) {
-      for (int num : numbers) {
-        System.out.print(num + " ");
-      }
-      System.out.println();
-      return;
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    for (int i = start; i <= N; i++) {
-      numbers[depth] = i;
-      combination(depth + 1, i + 1);
-    }
-  }
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
-    N = Integer.parseInt(input[0]);
-    M = Integer.parseInt(input[1]);
+		numbers = new int[M];
+		combination(0, 1);
 
-    numbers = new int[M];
-    combination(0, 1);
-  }
+	}
 }
