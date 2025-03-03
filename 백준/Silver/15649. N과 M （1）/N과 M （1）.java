9a899cf3264d1f71138a73b6ecf10898ca284d38
하승재 <117517535+SEUNGJAE-97@users.noreply.github.com>
2025-03-03 23:49:21 +0900

    [Silver III] Title: N과 M (1), Time: 2580 ms, Memory: 64092 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-  private static int N, M;
-  private static int[] numbers;
-  private static boolean[] visited;
-
-  private static void permutation(int depth) {
-    if (depth == M) {
-      for (int num : numbers) {
-        System.out.print(num + " ");
-      }
-      System.out.println();
-      return;
-    }
-
-    for (int i = 1; i <= N; i++) {
-      if (!visited[i]) {
-        visited[i] = true;
-        numbers[depth] = i;
-        permutation(depth + 1);
-        visited[i] = false;
-      }
-    }
-  }
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
-    N = Integer.parseInt(input[0]);
-    M = Integer.parseInt(input[1]);
-
-    numbers = new int[M];
-    visited = new boolean[N + 1];
-
-    permutation(0);
-  }
-
+	private static int N, M;
+	private static int[] numbers;
+	private static boolean[] visited;
+	private static void permutation(int cnt) {
+		if(cnt == M) {
+			for(int num : numbers) {
+				System.out.print(num);
+				System.out.print(" ");
+			}
+			System.out.println();
+			return;
+		}
+		for(int i = 1; i <= N; i ++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				numbers[cnt] = i;
+				permutation(cnt+1);
+				visited[i] = false;
+			}
+		}
+	}
+	public static void main(String[] args) throws Exception{	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[M];
+		visited = new boolean[N+1];
+		permutation(0);
+		
+	}
 }
