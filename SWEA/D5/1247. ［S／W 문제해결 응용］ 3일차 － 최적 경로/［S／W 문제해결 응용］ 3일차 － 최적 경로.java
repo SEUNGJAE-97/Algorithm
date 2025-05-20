f00b68e26ed4e3e8df5f948b8a20749cd511bef2
하승재 <117517535+SEUNGJAE-97@users.noreply.github.com>
2025-05-20 13:46:34 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,326 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,76 +1,75 @@
 import java.io.*;
 import java.util.*;
 
-class Node {
-	int x;
-	int y;
-
-	public Node(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-}
-
+// 1247 최적경로
 public class Solution {
-	private static int T, N;
-	private static Node[] customers;
-	private static int distance;
-	private static Node start, end;
-	private static boolean[] visited;
+  private static int T, N;
+  private static Node[] customer;
+  private static Node Home, Company;
+  private static int result;
+  private static boolean[] visited;
 
-	private static void dfs(int depth, int prevX, int prevY, int totalDist) {
-		// 모든 고객을 방문했으면 집까지 이동
-		if (depth == N) {
-			// 마지막으로 집 좌표까지 얼마나 걸리는지 계산
-			totalDist += Math.abs(prevX - end.x) + Math.abs(prevY - end.y);
-			distance = Math.min(distance, totalDist);
-			return;
-		}
+  private static void dfs(int depth, int prevX, int prevY, int distance) {
+    // 종료조건
+    if (depth == N) {
+      distance += Math.abs(prevX - Home.x) + Math.abs(prevY - Home.y);
+      result = Math.min(distance, result);
+      return;
+    }
+    for (int n = 0; n < N; n++) {
+      if (!visited[n]) {
+        // 방문처리
+        visited[n] = true;
 
-		for (int i = 0; i < N; i++) {
-			// 방문하지 않은 손님부터
-			if (!visited[i]) {
-				// 방문처리
-				visited[i] = true;
-				// 거리계산
-				int dist = Math.abs(prevX - customers[i].x) + Math.abs(prevY - customers[i].y);
-				// 다음 방문하지 않은 손님에게 찾아가고, 거리 갱신
-				dfs(depth + 1, customers[i].x, customers[i].y, totalDist + dist);
-				visited[i] = false;
-			}
-		}
-	}
+        int dist = Math.abs(prevX - customer[n].x) + Math.abs(prevY - customer[n].y);
+        // 재귀
+        dfs(depth + 1, customer[n].x, customer[n].y, distance+dist);
 
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        // 다시 복구
+        visited[n] = false;
+      }
 
-		T = Integer.parseInt(br.readLine());
+    }
+  }
 
-		for (int tc = 1; tc <= T; tc++) {
+  static class Node {
+    int x;
+    int y;
 
-			N = Integer.parseInt(br.readLine());
-			customers = new Node[N];
-			distance = Integer.MAX_VALUE;
-			visited = new boolean[N];
+    public Node(int x, int y) {
+      super();
+      this.x = x;
+      this.y = y;
+    }
+  }
 
-			// 첫 번째 줄 (회사, 집, 사람들) 정보를 처리
-			String[] split = br.readLine().split(" ");
 
-			start = new Node(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
-			end = new Node(Integer.parseInt(split[2]), Integer.parseInt(split[3]));
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    // 0. 데이터 입력
+    T = Integer.parseInt(br.readLine().trim());
 
-			for (int i = 0, idx = 4; i < N; i++, idx += 2) {
-				customers[i] = new Node(Integer.parseInt(split[idx]), Integer.parseInt(split[idx + 1]));
-			}
-			dfs(0, start.x, start.y, 0);
+    for (int tc = 1; tc <= T; tc++) {
+      N = Integer.parseInt(br.readLine().trim());
+      result = Integer.MAX_VALUE;
+      customer = new Node[N];
+      visited = new boolean[N];
 
-			sb.append("#").append(tc).append(" ").append(distance).append("\n");
+      String[] split = br.readLine().split(" ");
 
-		}
-		System.out.println(sb);
+      Company = new Node(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+      Home = new Node(Integer.parseInt(split[2]), Integer.parseInt(split[3]));
 
-	}
+      for (int i = 0, idx = 4; i < N; i++, idx += 2) {
+        customer[i] = new Node(Integer.parseInt(split[idx]), Integer.parseInt(split[idx + 1]));
+      }
+      // 1. dfs탐색
+      // depth, prevX, prevY, distance
+      dfs(0, Company.x, Company.y, 0);
+      sb.append("#").append(tc).append(" ").append(result).append("\n");
+    }
+
+    System.out.println(sb);
+  }
 }
